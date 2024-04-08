8c827c50bfff50eac2883ddf5fe4586e9232194d
ericaphann <ericaphann@yahoo.com>
2024-04-08 12:05:37 -0400

    added trash button, but image not working

--- Earth Day/ocean.js
+++ Earth Day/ocean.js
@@ -1,19 +1,25 @@
 var id = null;
 function myMove() {
-  var elem = document.getElementById("boat");
-  var pos = -50;
-  var elStyle = window.getComputedStyle(elem);
-  var rightValue = elStyle.getPropertyValue("right").replace("px","")
+  var boat = document.getElementById("boat");
+  var pos = 0;
   clearInterval(id);
   id = setInterval(frame, 10);
   function frame() {
-    if (pos == 1250) {
+    if (pos == 200) {
       clearInterval(id);
     } else {
       pos++;
-      elem.style.left = pos + 'px';
+      boat.style.left = pos + 'px';
     }
   }
+}
+
+function clickTrash(){
+  const trash = document.getElementById("trash");
+  trash.addEventListener("click", function(e) {
+    console.log("The button was clicked");
+  });
 
 }
 myMove();
+clickTrash();
